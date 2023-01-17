16249c61755206010abc88423395379229ecd0ac
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2023-01-17 12:09:54 +0500

    First Commit

---
+++ AbduRahimApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AbduRahimApp
+{
+    class Program
+    {
+        static void Main() 
+        {
+            User user = new User("Xabibulla", "Xolmatov");
+            user.Gender = (Gender)5;
+            Console.WriteLine(user.Gender);
+        }
+    }
+}
---
+++ AbduRahimApp/User.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+namespace AbduRahimApp
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+
+        public User(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
